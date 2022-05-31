393588dd39129f41f74226933d85a6c6c7b61789
JMaria <jmariadelacruzsanchez@gmail.com>
2022-06-01 01:15:31 +0200

    deploy scripts

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,4 +1,4 @@
-const { networkConfig } = require("../helper-hardhat-config") // to manage different chains
+const { networkConfig, developmentChains } = require("../helper-hardhat-config") // to manage different chains
 const { network } = require("hardhat")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
@@ -7,8 +7,15 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const chainId = network.config.chainId
 
     // Use chainId to select different arguments depending on the chain we are deploying -> MULTICHAIN 
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
 
+    log("Deploying FundMe and waiting for confirmations...")
     if (chainId == 31337) { // hardhat local network -> In this Network it does not exist a priceAggregator so we need to deploy our MockV3Aggregator in this network to use it
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
@@ -19,7 +26,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // When deploying on localhost or hardhat network we want to use Mocks
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [],
-        logs: true,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
     })
+    log("--------------------------------")
 }
+
+module.exports.tags = ["all", "fundme"]
