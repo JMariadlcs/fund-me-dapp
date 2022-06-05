a88fac6d6c0ef71eaf84926ce789da861fe56365
JMaria <jmariadelacruzsanchez@gmail.com>
2022-06-05 02:03:39 +0200

    contract update

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,8 +9,8 @@ require("dotenv").config();
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || ""
-const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL 
+const PRIVATE_KEY = process.env.PRIVATE_KEY 
 const ETHERSCAN_API_KEY =  process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
@@ -38,7 +38,7 @@ module.exports = {
     coinmarketcap: COINMARKETCAP_API_KEY,
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: ETHERSCAN_API_KEY,
   },
   namedAccounts: {
     deployer: {
